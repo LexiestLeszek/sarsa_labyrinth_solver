4981fd024cb3da682f88a5b6ab405990b9ceeefb
Leszek Mielnikow <63610219+LexiestLeszek@users.noreply.github.com>
2023-11-23 08:26:38 +0100

    improved main algorithm
    Changes include:
    - Refactored code structure for readability and modularity
    - Enhanced exploration-exploitation strategy for action selection
    - Improved reward scheme for better learning convergence
    - Implemented visualization snapshots for tracking learning progress
    - Enhanced SARSA learning loop for more efficient Q-value updates
    - Added flexibility for maze dimensions and diverse environments

--- play.py
+++ play.py
@@ -1,9 +1,12 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import random
+import copy
+from celluloid import Camera
 
 
 def get_states(m):
-    TS, IS, FS = ['c', 'nv', 'oob'], [], []
+    TS, IS, FS = ['f', 'nv', 'oob'], [], []
 
     for i in range(0, len(m)):
         for j in range(0, len(m[i])):
@@ -54,6 +57,12 @@ class play:
         self.state_space = get_state_space(self.maze)
 
         self.action_value_function = init_q(self.state_space, self.action_space)
+        self.gif = []
+        self.rewards = []
+        self.n_succ = []
+
+        self.fig, self.ax = plt.subplots()
+        self.camera = Camera(self.fig)
 
     def check_move(self, prev_state, curr_state):
         """
@@ -102,24 +111,29 @@ class play:
                 else:
                     return vel_action
         else:
-            greedy_action = None
-            tup = None
+            while True:
+                tup = tuple(random.sample(self.inc_action_space, 2))
+                if prev_action[0] + tup[0] == 0 and prev_action[1] + tup[1] == 0:
+                    # print('here')
+                    continue
+                elif abs(prev_action[0] + tup[0]) >= 5 or abs(prev_action[1] + tup[1]) >= 5:
+                    # print('here_again')
+                    continue
+                else:
+                    break
+            greedy_action_value = self.action_value_function[prev_state, (prev_action[0] + tup[0], prev_action[1] + tup[1])]
             for a1 in self.inc_action_space:
                 for a2 in self.inc_action_space:
                     if (prev_action[0] + a1) == 0 and (prev_action[1] + a2) == 0:
                         continue
-                    elif abs(prev_action[0] + a1) >= 5 or (prev_action[1] + a2) >= 5:
+                    elif abs(prev_action[0] + a1) >= 5 or abs(prev_action[1] + a2) >= 5:
                         continue
                     else:
-                        if greedy_action is None:
-                            greedy_action = self.action_value_function[
-                                prev_state, (prev_action[0] + a1, prev_action[1] + a2)]
-                            continue
-                        if greedy_action < self.action_value_function[
-                            prev_state, (prev_action[0] + a1, prev_action[1] + a2)]:
+                        if greedy_action_value < self.action_value_function[prev_state, (prev_action[0] + a1, prev_action[1]
+                                                                                                        + a2)]:
+                            greedy_action_value = self.action_value_function[prev_state, (prev_action[0] + a1, prev_action[1]
+                                                                                    + a2)]
                             tup = (a1, a2)
-                    if tup is None:
-                        tup = tuple(random.sample(self.inc_action_space, 2))
             return tup
 
     def reward_function(self, prev_state, curr_state):
@@ -131,6 +145,7 @@ class play:
         :return: rewards obtained
         """
         move = self.check_move(prev_state, curr_state)
+        r = 0
         if move == 'f':
             r = 10
         elif move == 'oob':
@@ -138,11 +153,75 @@ class play:
         elif move == 'nv':
             r = -10
         elif move == 'v':
-            r = 1
+            r = -1
         return r
 
-    def q(self):
-        pass
-
     def td_sarsa(self):
-        pass
+        count = 0
+        gif_counter = 0
+        while count <= self.n_trials:
+            curr_state = random.sample(self.IS, 1)[0]
+            v = (0, 0)
+            action = self.take_action(v, curr_state)
+            v = (v[0] + action[0], v[1] + action[1])
+            rew = 0
+            states = [curr_state]
+            while True:
+                new_state = (curr_state[0] + v[0], curr_state[1] + v[1])
+                move = self.check_move(curr_state, new_state)
+                if move == 'oob':
+                    new_state = (-1, -1)
+                new_rew = self.reward_function(curr_state, new_state)
+                new_action = self.take_action(v, new_state)
+                v1 = (v[0] + new_action[0], v[1] + new_action[1])
+                rew = rew + new_rew
+                self.action_value_function[curr_state, v] = (self.action_value_function[curr_state, v]
+                                                             + self.alpha * (new_rew + self.gamma
+                                                                             * self.action_value_function[new_state, v1]
+                                                                             - self.action_value_function[
+                                                                                 curr_state, v]))
+                curr_state = new_state
+                v = v1
+                states.append(curr_state)
+                self.rewards.append(rew)
+                if move in self.TS:
+                    if move == 'f':
+                        count += 1
+                    gif_counter += 1
+                    if not gif_counter % 1000:
+                        self.take_snap(states)
+                    if not gif_counter % 10000:
+                        print('Num trials = {} \t Num successes = {} \t Prop success = {}'.format(gif_counter, count,
+                                                                                                  round(count/gif_counter, 3)))
+                    self.n_succ.append(round(count/gif_counter, 3))
+                    break
+            if self.train and self.eps_decay:
+                self.eps = self.eps - count / 10000 if self.eps > 0.01 else 0.01
+
+        return self.camera, self.rewards, self.n_succ
+
+    def take_snap(self, states):
+        m1 = copy.deepcopy(self.maze)
+        nrows, ncols = self.mw, self.ml
+        image = np.zeros(nrows * ncols)
+        image = image.reshape((nrows, ncols))
+        for s in states:
+            if s != (-1, -1):
+                m1[s] = 'g'
+        for i in range(0, nrows):
+            for j in range(0, ncols):
+                if m1[i][j] == 's':
+                    image[i][j] = 0
+                if m1[i][j] == '0':
+                    image[i][j] = 1
+                if m1[i][j] == 'x':
+                    image[i][j] = 2
+                if m1[i][j] == 'f':
+                    image[i][j] = 3
+
+        self.gif.append(image)
+        self.ax.imshow(image)
+        self.ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=1)
+        self.ax.set_xticks(np.arange(-.5, ncols, 1))
+        self.ax.set_yticks(np.arange(-.5, nrows, 1))
+        self.camera.snap()
